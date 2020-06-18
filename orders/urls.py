0b70800a5fa14868ab339fb26faa169f0f154c18
VishiK-on-github <vck281000@gmail.com>
2020-06-18 21:46:18 +0530

    second commit: login and registration page done

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_view, name="login_view"),
+    path("", views.index, name="index"),
     path("login/", views.login_view, name="login_view"),
-    path("register/", views.register_view, name="register_view")
+    path("register/", views.register_view, name="register_view"),
+    # path("homepage/", views.homepage_view, name="homepage_view")
+    path("logout/", views.logout_view, name="logout_view")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,38 +1,54 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from orders.models import RegularPizza, SicilianPizza, Salad, Sub, Pasta, DinnerPlatter, Topping
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if request.user.is_authenticated:
+        return render(request, "orders/homepage.html", {"title": "Homepage"})
+    else:
+        return render(request, "orders/login.html", {"title": "Login"})
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST("username")
-        password = request.POST("password")
-        user = authenticate(request, username=username, password=password)
+        data = request.POST.copy()
+        username = data.get("username")
+        password = data.get("password")
+        user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
             # Go to Homepage. 
+            return render(request, "orders/homepage.html", {"title": "Homepage"})
         else:
-            return render(request, "orders/login.html", {"title": "Login"})
+            return render(request, "orders/login.html", {"title": "Login", "Message": "Username or password is incorrect !"})
     elif request.method == "GET":
         return render(request, "orders/login.html", {"title": "Login"})
 
 def logout_view(request):
     logout(request)
-    return render(request, "orders/login.html")
+    return HttpResponseRedirect(reverse("index"))
 
 def register_view(request):
     if request.method == "POST":
-        '''username = request.POST("username")
-        password = request.POST("password")
-        confirm_password = request.POST("cpassword")
-        email = request.POST("email")
-        first_name = request.POST("fname")
-        last_name = request.POST("lname")'''
+        data = request.POST.copy()
+        username = data.get("username")
+        password = data.get("password")
+        confirm_password = data.get("cpassword")
+        email = data.get("email")
+        first_name = data.get("fname")
+        last_name = data.get("lname") 
+
+        if password != confirm_password:
+            return render(request, "orders/register.html", {"title": "Register", "Message": "The Passwords dont match !"})
+
+        elif username == '' or password == '' or confirm_password == '' or email == '' or first_name == '' or last_name == '':
+            return render(request, "orders/register.html", {"title": "Register", "Message": "Any one of the fields is empty !"})
+
+        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        user.save()
+        return render(request, "orders/login.html")
 
     elif request.method == "GET":
         return render(request, "orders/register.html", {"title": "Register"})
